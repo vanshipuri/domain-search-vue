e14e8a1f439ba827bfd6acd902b672da13ef61a9
Vanshi Puri <vanshipuri76@gmail.com>
2025-07-23 13:57:20 +0530

    use port from environment configuration

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,12 @@ const whoisapi = require("./routes/whoisapi");
 const trackedRoutes = require("./routes/tracked");
 const notifyRoutes = require("./routes/notify");
 const runDailyNotifier = require("./cron/notifyCron");
-const authRoutes = require('./routes/auth');
-const historyRoutes = require('./routes/history'); 
-const protectedRoutes = require('./routes/protected');
+const authRoutes = require("./routes/auth");
+const historyRoutes = require("./routes/history");
+const protectedRoutes = require("./routes/protected");
 
 const app = express();
-const PORT = 5000;
+const PORT = Number.parseInt(process.env.APP_PORT || 5000);
 
 app.use(cors());
 app.use(express.json());
@@ -19,14 +19,12 @@ app.use(express.json());
 app.use("/api/track", trackedRoutes);
 app.use("/api/whois", whoisapi);
 app.use("/api/notify", notifyRoutes);
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 app.use("/api", historyRoutes);
-app.use('/api', protectedRoutes);
-
+app.use("/api", protectedRoutes);
 
 runDailyNotifier();
 
-
-app.listen(5000, () => {
-  console.log(`Server running on http://localhost:${5000}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
